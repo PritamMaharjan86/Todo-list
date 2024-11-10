1f07deaf9485b58b4b97e487ff12ac13c20c672d
Pritam Maharjan <primgdev@gmail.com>
2024-11-10 16:49:23 +1100

    backend running

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -30,8 +30,8 @@ function App() {
 
 
   return (
-    <div className="text-lg text-center font-TNanti flex justify-center items-center">
-      <div className='border-2 border-black p-8 w-1/6 h-full rounded-lg shadow-2xl bg-slate-100'>
+    <div className="text-lg text-center font-TNanti flex justify-center items-center ">
+      <div className='border-2 border-black p-10 h-full rounded-lg shadow-2xl bg-slate-100'>
         <h1>Todo List</h1>
 
         <Input
