ab75ad287697434bcc50c1c3f5a16de676169806
Pritam Maharjan <primgdev@gmail.com>
2024-11-17 21:19:43 +1100

    checking

--- Frontend/src/components/button.js
+++ Frontend/src/components/button.js
@@ -1,18 +1,18 @@
-import React from 'react'
+// import React from 'react'
 
-const Button = ({ label, onClick }) => {
+// const Button = ({ label, onClick }) => {
 
 
-    return (
-        <div>
-            <button
-                className='border-2 border-black rounded-md p-1 mt-5 text-sm '
-                onClick={onClick}
+//     return (
+//         <div>
+//             <button
+//                 className='border-2 border-black rounded-md p-1 mt-5 text-sm '
+//                 onClick={onClick}
 
-            >{label}</button>
+//             >{label}</button>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Button
+// export default Button
