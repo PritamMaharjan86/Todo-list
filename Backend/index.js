215da14b44932845fa7cb0dcc46e845ce7c7a0a6
Pritam Maharjan <primgdev@gmail.com>
2026-06-23 19:40:44 +1000

    adding server and mysql database

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const TodoRouter = require('./routes/TodoRouter')
+import mysql from "mysql2";
 
-dotenv.config();
+//connect to mysql server
 
-require('./models/Database');
-
-app.listen(3001, () => console.log('Server is running on'));
-
-app.use(cors());
-app.use(bodyParser.json);
-app.use('/todos', TodoRouter)
-
-app.get('/', (req, res) => {
-    res.send('Your server is running!');
+const database = await mysql.createConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: "",
 });
+
+//create database
+
+//create a table
+
+//perform curd operation
