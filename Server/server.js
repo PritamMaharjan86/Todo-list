5c58356ee9a213d27272827a2583347e0be474ee
Pritam Maharjan <primgdev@gmail.com>
2026-07-20 20:24:34 +1000

    post api done  sql

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config.js";
-import "./database.js";
+import database from "./database.js";
 
 const PORT = process.env.PORT;
 
@@ -12,6 +12,25 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is working...");
+});
+
+app.post("/todos", (req, res) => {
+  const { title } = req.body;
+
+  database.query(
+    "INSERT INTO todos(title) VALUES(?)",
+    [title],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        id: result.insertId,
+        title: title,
+      });
+    },
+  );
 });
 
 app.listen(process.env.PORT, () => {
